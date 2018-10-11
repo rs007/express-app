c95f4e534e8885cb07c0c0b038de7078cda1ca63
rs007 <rohitsingh9876@gmail.com>
2018-10-11 23:36:20 +0530

    Express app set up

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+	res.send('Hello world...');
+});
+
+app.get('/user', (req, res)=>{
+	res.status(200).send({
+		message:"User endpoint....",
+	})
+});
+
+app.listen(PORT, ()=>{
+	console.log(`server is running on port--${PORT}`)
+});
